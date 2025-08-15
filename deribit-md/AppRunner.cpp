4c5bb6d789f3957c9c0d90652f9c40f73911871a
TuxedoFish <harryliversedge@gmail.com>
2025-08-15 13:51:21 +0100

    read raw fix into fix messages to process

--- deribit-md/AppRunner.cpp
+++ deribit-md/AppRunner.cpp
@@ -3,8 +3,100 @@
 AppRunner::AppRunner(const SimpleConfig& config) : config_{ config } {
 }
 
+std::string getMonthDayString(int dayOrMonth) {
+    if (dayOrMonth > 9) {
+        return std::to_string(dayOrMonth);
+    }
+    else {
+        return "0" + std::to_string(dayOrMonth);
+    }
+}
+
+tm getDateFromString(std::string dateString) {
+    tm tm = {};
+    std::istringstream ss(dateString);
+
+    // Parse the date string using std::get_time
+    ss >> std::get_time(&tm, "%Y%m%d");
+
+    if (ss.fail()) {
+        std::cout << "Date parsing failed!" << std::endl;
+        return tm;
+    }
+    return tm;
+}
+
 int AppRunner::runProcessRawMarketdata() {
-    std::cout << "Reading from raw FIX messages from: " << config_.getString("md_raw_fix_file_path") << std::endl;
+    // Fetch configuration
+    std::string rawFixCapturesLoc = config_.getString("md_raw_fix_file_path");
+    std::string startDateStr = config_.getString("start_date", "");
+    std::string endDateStr = config_.getString("end_date", "");
+
+    if (startDateStr == "" || endDateStr == "") {
+        std::cout << "Required start_date and end_date missing." << std::endl;
+    }
+
+    std::cout << "Processing from " << startDateStr << " until " << endDateStr << std::endl;
+    tm startDate = getDateFromString(startDateStr);
+    tm endDate = getDateFromString(endDateStr);
+    tm currentDate = startDate;
+    MessageProcessor processor{};
+
+    // Main app loop
+    while (true) {
+        std::string filePath = rawFixCapturesLoc + kPathSeparator + std::to_string(1900 + currentDate.tm_year) 
+            + kPathSeparator + getMonthDayString(currentDate.tm_mon + 1)
+            + kPathSeparator + getMonthDayString(currentDate.tm_mday) + ".txt";
+        std::ifstream file(filePath);
+        if (!file.good()) {
+            if (currentDate.tm_mday < 28) {
+                std::cout << "Could not find: " << filePath << " exiting." << std::endl;
+                break;
+            }
+
+            // Try the next month / year
+            currentDate.tm_mday = 1;
+            currentDate.tm_mon += 1;
+            if (currentDate.tm_mon > 11) {
+                currentDate.tm_mon += 1;
+                currentDate.tm_year += 1;
+            }
+            filePath = rawFixCapturesLoc + kPathSeparator + std::to_string(1900 + currentDate.tm_year)
+                + kPathSeparator + getMonthDayString(currentDate.tm_mon + 1)
+                + kPathSeparator + getMonthDayString(currentDate.tm_mday) + ".txt";
+        }
+
+        file = std::ifstream(filePath);
+        if (!file.good()) {
+            std::cout << "Could not find: " << filePath << " exiting." << std::endl;
+            break;
+        }
+
+        auto end = std::chrono::system_clock::now();
+        std::time_t time = std::chrono::system_clock::to_time_t(end);
+        std::cout << "Processing " << filePath << " started at " << std::ctime(&time) << std::endl;
+        std::string line;
+        while (std::getline(file, line))
+        {
+            if (line.empty())
+                continue;
+
+            int splitIdx = line.find("|");
+            std::string msgIdentifier = line.substr(0, splitIdx);
+            std::string msgStr = line.substr(splitIdx + 1);
+
+            FIX::Message msg(msgStr);
+
+            processor.process(msg);
+        }
+        currentDate.tm_mday += 1;
+        if (currentDate.tm_year == endDate.tm_year && currentDate.tm_mon == endDate.tm_mon && currentDate.tm_mday == endDate.tm_mday) {
+            std::cout << "Finished processing." << std::endl;
+            break;
+        }
+    }
+
+    // Cleanup
     return 0;
 }
 
--- deribit-md/AppRunner.h
+++ deribit-md/AppRunner.h
@@ -2,16 +2,29 @@
 
 #include <iostream>
 #include <fstream>
+#include <ctime>
+#include <iomanip>
+#include <iostream>
+#include <chrono>
+#include <sstream>
 #include "Application.h"
 #include "SimpleConfig.h"
 #include "ApplicationPersister.h"
 #include "CmdLineOptions.h"
 #include "NullLogFactory.h"
+#include "MessageProcessor.h"
 #include "quickfix/FileStore.h"
 #include "quickfix/FileLog.h"
 #include "quickfix/SocketInitiator.h"
 #include "quickfix/SessionSettings.h"
 #include "quickfix/ThreadedSSLSocketInitiator.h"
+
+const std::string kPathSeparator =
+#ifdef _WIN32
+"\\";
+#else
+"/";
+#endif
 
 class AppRunner
 {
